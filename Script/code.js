d46844c545aab402e53701949086c60ae7afb240
n0tnish <nishaatgafieldien@gmail.com>
2023-11-24 15:08:17 +0200

    added

---
+++ Script/code.js
@@ -0,0 +1,8 @@
+let numbers = [1, 2, 3, 4, 5];
+let currentIndex = 0;
+function displayNextElement() {
+    if (currentIndex >= numbers.length) {
+        currentIndex = 0;
+        let nextElement = number[currentIndex];
+    }
+}
